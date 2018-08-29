9182a631ced71bfcb7cdf77b352be861d337d6d4
Steven Gorrell <stevengor@gmail.com>
2018-08-29 09:14:59 -0500

    Adding VPC Peer.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aws-terraform-vpc_peer
+
+This repository contains terraform modules that can be used to deploy a VPC Peer.
+
+## Module Listing
+- [VPC Peer](./modules/vpc_peer) - A terraform module for creating a VPC Peer within the same AWS account. 
+- [VPC Peer Cross Account](./modules/vpc_peer_cross_account) - A terraform module for creating a VPC Peer across different AWS accounts.
---
+++ modules/vpc_peer/variables.tf
@@ -0,0 +1,45 @@
+variable "environment" {
+  description = "Application environment for which this network is being created. one of: ('Development', 'Integration', 'PreProduction', 'Production', 'QA', 'Staging', 'Test')"
+  type        = "string"
+  default     = "Development"
+}
+
+variable "tags" {
+  description = "Custom tags to apply to all resources."
+  type        = "map"
+  default     = {}
+}
+
+variable "peer_owner_id" {
+  description = "The AWS account ID of the owner of the peer VPC. Defaults to the account ID the AWS provider is currently connected to. (OPTIONAL)"
+  type        = "string"
+  default     = ""
+}
+
+variable "peer_vpc_id" {
+  description = "The ID of the VPC with which you are creating the VPC Peering Connection."
+  type        = "string"
+}
+
+variable "vpc_id" {
+  description = "The ID of the requester VPC."
+  type        = "string"
+}
+
+variable "auto_accept" {
+  description = "Accept the peering (both VPCs need to be in the same AWS account). (OPTIONAL)."
+  type        = "string"
+  default     = false
+}
+
+variable "peer_region" {
+  description = "The region of the accepter VPC of the [VPC Peering Connection]. auto_accept must be false, and use the aws_vpc_peering_connection_accepter to manage the accepter side. (OPTIONAL)"
+  type        = "string"
+  default     = ""
+}
+
+variable "allow_remote_vpc_dns_resolution" {
+  description = "Allow a local VPC to resolve public DNS hostnames to private IP addresses when queried from instances in the peer VPC."
+  type        = "string"
+  default     = true
+}
